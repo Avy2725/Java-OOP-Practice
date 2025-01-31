b51ec4730c08c364b565dff702ca04bea09167ab
Avy <avycarlos2005@gmail.com>
2025-01-31 08:07:37 +0800

    Created carKo

---
+++ carKo.java
@@ -0,0 +1,31 @@
+public class carKo {
+  // fields, properties and attributes
+  private String brand;
+  private int CGeer;
+  private int speed;
+
+
+  // constructor
+  public carKo (String brand,int CGeer, int speed) 
+  {// this = assgigns it to the paramiter ex bathrooms to bathrooms
+    this.brand = brand;
+    this.CGeer = CGeer;
+    this.speed = speed;
+  }
+
+  // global funcations/methods that can be used by any new Object, house1 & house2
+
+
+  public static void main(String[] args) {
+    //  objects are anything that is real
+    //  classes are blue-prins of an object
+
+    //  Data abstraction 
+    //  Encapsulation
+    //  Modularity
+    //  Polymorphism, and inheritance
+
+
+  }
+}
+
