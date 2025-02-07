d48299e33135889d16453cc43b8c4f245249fd3b
Avy <avycarlos2005@gmail.com>
2025-02-07 09:38:09 +0800

    Convert into oop passByValueEx.java

--- Examples/passByValueEx.java
+++ Examples/passByValueEx.java
@@ -1,19 +1,40 @@
-public class passByValueEx {
-  // This method attempts to modify the value of the parameter 'num'
-  public static void modVall(int num) {
-    // Assigning a new value to 'num'
-    num = 20;
-    // Printing the value of 'num' within the method
-    System.out.println("After calling the method, the value of num is: " + num);
-  }
-    
-  public static void main(String[] args) {
-    // Initializing 'num' with a value of 10
-    int num = 10;
-    // Printing the value of 'num' before calling the method
-    System.out.println("Before calling the method, the value of num is: " + num);
+// Class representing a Number with a single integer field 'num'
+class Number {
+  // Private field to store the integer value
+  private int num;
 
-    // Calling the method 'modVall' and passing 'num' as an argument
-    modVall(num);
+  // Constructor to initialize the 'num' field
+  public Number(int num) {
+    this.num = num;
+  }
+
+  // Getter method to retrieve the value of 'num'
+  public int getNum() {
+    return num;
+  }
+
+  // Setter method to set the value of 'num'
+  public void setNum(int num) {
+    this.num = num;
+  }
+
+  // Method to modify the value of 'num' to 20
+  public void modVall() {
+    this.num = 20;
+    // Printing the value of 'num' after modification
+    System.out.println("After calling the method, the value of num is: " + this.num);
   }
 }
+
+public class passByValueEx {
+  public static void main(String[] args) {
+    // Creating an instance of Number with initial value 10
+    Number number = new Number(10);
+    // Printing the value of 'num' before calling the method
+    System.out.println("Before calling the method, the value of num is: " + number.getNum());
+
+    // Calling the method 'modVall' on the instance 'number'
+    number.modVall();
+  }
+}
+
