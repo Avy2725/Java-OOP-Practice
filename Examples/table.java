80fe6cb61189c9690a58fca06d4301552377c670
Avy <avycarlos2005@gmail.com>
2025-02-21 09:14:55 +0800

    Updated table class and table.java

--- Examples/table.java
+++ Examples/table.java
@@ -13,14 +13,23 @@ public class table {
       System.out.println("Invalid number format. Please provide valid integers.");
       return;
     }
+    int[][] table = new int[x][y];
+    for (int i = 0; i < x; i++) {
+      for (int j = 0; j < y; j++) {
+      table[i][j] = (i + 1) * (j + 1);
+      }
+    }
 
     System.out.println("Multiplication Table: " + x + " x " + y);
-    for (int i = 1; i <= x; i++) {
-      for (int j = 1; j <= y; j++) {
-        System.out.print("\t" +(i * j));
+    System.out.println("--------------------------------------");
+    for (int[] row : table) {
+      for (int value : row) {
+      System.out.print(value + "\t");
       }
       System.out.println();
+      System.out.println();
     }
+
 
 }
 }
