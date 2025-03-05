d082b9a331c1104b0fd72860dd02cd7492167a2f
Avy <avycarlos2005@gmail.com>
2025-03-05 08:40:00 +0800

    Updated files

---
+++ Examples/Employee Example/superExample.java
@@ -0,0 +1,16 @@
+// public class superExample extends Employeee {
+//     public String department;
+//     public superExample(String name, double salary, String department) {
+//         super(name, salary);
+//         this.department = department;
+//     }
+
+//     public String getDetails() {
+//         return super.getDetails() + " " + department;
+//     }
+//     public static void main(String[] args) {
+//         superExample e = new superExample("John", 50000, "HR");
+//         System.out.println(e.getDetails());
+//     }
+  
+// }
--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Examples Folder: Is For Other Exmaples Applying the code found in OOP Notes Fold
 
 Lab Folder: Is for My Lab Code Examples.
 
-OOP Notes Folder: Is For OOP Fundamental Notes And For Other Helpful Notes. 
+OOP Notes Folder: Is For OOP Fundamental Notes And For Other Helpful Notes.
+
+Practice Folder: Is for Java Practice Files.
